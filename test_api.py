ff215015fc06255b680b5254191f227a5ce1a18a
lisawei <187055815@qq.com>
2013-12-15 12:03:21 +0800

    Create test_api.py

---
+++ test_api.py
@@ -0,0 +1,25 @@
+import random
+import unittest
+from base import RequestApi
+import json
+
+class TestApiFunctions(unittest.TestCase):
+    def setUp(self):
+        self.seq = range(10)
+
+    def test_not_null(self):
+        data = RequestApi.get_json('/book/subject/1220562', {'alt': 'json'})
+        self.assertIsNotNone(data)
+
+    def test_title_required(self):
+        data = RequestApi.get_json('/book/subject/1220562', {'alt': 'json'})
+        self.assertIsNotNone(data)
+        self.assertIsNotNone(data["title"])
+
+    def test_sample(self):
+        data = RequestApi.get_json('/book/subject/1220562', {'alt': 'json'})
+        #self.assertIsNone(data)
+
+
+if __name__ == '__main__':
+    unittest.main()
